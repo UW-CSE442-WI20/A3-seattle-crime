7d4aab796cb416918da21328cb8d6b6bf5caf0ee
Trey Tamaki <tgtamaki@uw.edu>
2020-02-13 02:06:02 -0800

    change header and add write-up space

--- src/Header.js
+++ src/Header.js
@@ -3,22 +3,30 @@ import styled from "styled-components";
 
 export default function Header() {
     const Header = styled.div`
-    background: pink;
+    background: lightgray;
+    
   `;
 
     const Title = styled.div`
     margin: 0 0;
-    padding: 0px 800px;
     font-size: 1.5em;
     display: inline-block;
-
+    
   `;
+
+  const Logo = styled.div`
+  margin: 2;
+
+  
+
+`;
 
 
     return(
 
       <Header>
-        <Title>CRIME IN SEATTLE</Title>
+        <img src="http://www.seattle.gov/Images/Council/Logos/City-of-Seattle-Logo_Color-on-Transparent.png" height="50px" style={{margin: "5px"}}></img>
+        <Title>Crime in Seattle, WA</Title>
       </Header>
     );
 

c6b0a983f015bdd59ca38c99ca90835b225e0283
Trey Tamaki <tgtamaki@uw.edu>
2020-02-13 00:57:53 -0800

    add pie chart

--- src/DataContainer.js
+++ src/DataContainer.js
@@ -6,8 +6,10 @@ import ReactMapGL, {Marker} from "react-map-gl";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 
-import PieChart from 'react-minimal-pie-chart';
+// import PieChart from 'react-minimal-pie-chart';
 import BarChart from 'react-bar-chart';
+
+import {Pie, Bar} from 'react-chartjs-2';
 
 
 
@@ -21,42 +23,114 @@ const Headline = styled.div`
     text-align: center;
 `;
 
-var pieData = [
 
-  ];
 
-const barData = [
-    {text: '1990', value: 500},
-    {text: '1991', value: 300}
-  ];
 
 function valuetext(value) {
   return `${value}°C`;
 }
-//     const InfoBox = styled.div`
-//           height: 400px;
-//           width: 400px;
-//           margin: 20px 0px 0px 10px;
-//           background: white;
-//    `;
-//    const Box = styled.div`
-//              height: 200px;
-//              width: 350px;
-//              background: white;
-//       `;
+
+
+
+// const piedata = {
+// 	labels: [
+// 		'Red',
+// 		'Blue',
+// 		'Yellow'
+// 	],
+// 	datasets: [{
+// 		data: [300, 50, 100],
+// 		backgroundColor: [
+// 		'#FF6384',
+// 		'#36A2EB',
+// 		'#FFCE56'
+// 		],
+// 		hoverBackgroundColor: [
+// 		'#FF6384',
+// 		'#36A2EB',
+// 		'#FFCE56'
+// 		]
+// 	}]
+// };
+
+
+// bar data
+
+// labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+// datasets: [
+//   {
+//     label: 'My First dataset',
+//     backgroundColor: 'rgba(255,99,132,0.2)',
+//     borderColor: 'rgba(255,99,132,1)',
+//     borderWidth: 1,
+//     hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+//     hoverBorderColor: 'rgba(255,99,132,1)',
+//     data: [65, 59, 80, 81, 56, 55, 40]
+//   }
+// ]
+
+
+var pieData = {};
+var barData = {};
 
 export default class LeftContainer extends PureComponent {
-    formatData() {
-        pieData = [];
-        for (const crime in this.props.categoryData) {
-            const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    formatPieData() {
+        // pieData = {
+        //     labels: [],
+        //     datasets: [{
+        //         data: [],
+        //         backgroundColor: [], 
+        //         hoverBackgroundColor: []
+        //     }]
+        // };
+        let labels = [];
+        let data = [];
+        let color = [];
 
-            pieData.push({
-                title: crime,
-                value: this.props.categoryData[crime],
-                color: randomColor
-            })
+        let count = 0;
+        for (const crime in this.props.categoryData) {
+            count++;
+            if (count < 20) {
+                const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+                labels.push(crime);
+                data.push(this.props.categoryData[crime]);
+                color.push(randomColor);
+            }
         }
+
+        pieData = {
+            labels: labels,
+            datasets: [{
+                data: data,
+                backgroundColor: color, 
+                hoverBackgroundColor: color
+            }]
+        };
+        console.log(pieData)
+    }
+
+    formatBarData() {
+        let labels = [];
+        let data = [];
+
+        for (const year in this.props.categoryData) {
+            labels.push(year);
+            data.push(this.props.categoryData[year]);
+        }
+
+        barData = {
+            labels: labels,
+            datasets: [
+            {
+                label: 'Crimes per Year',
+                backgroundColor: 'rgba(255,99,132,0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                hoverBorderColor: 'rgba(255,99,132,1)',
+                data: data
+            }
+        ]};
     }
 
     render() {
@@ -64,7 +138,8 @@ export default class LeftContainer extends PureComponent {
         if (this.props.markers.length === 0 || !this.props.categoryData) {
             return <div></div>;
         } else {
-            this.formatData();
+            this.formatPieData();
+            this.formatBarData();
 
             return(
                 <div>
@@ -76,18 +151,15 @@ export default class LeftContainer extends PureComponent {
                     <p>Longitude: {this.props.markers[0].longitude}</p>
                     <p>Radius: {this.props.radius}</p>
                     <p>Crime Score: {}</p>
-                    <PieChart data={pieData} />
-                    <BarChart style={{margin:"20px"}}
-                        height={500}
-                        width={document.getElementById("right-col").offsetWidth}
-                        data={barData}
-                        ylabel='Number of Crimes'
-                        xlabel='Year'
-                    />
+                    <div>
+                        <h2>Crimes per Area</h2>
+                    <Pie data={pieData} 
+                    height={500}/>
+                    </div>
+                 
                 </div>
             );
         }
     }
-
-
 }
+/* <Bar data={barData} /> */
